0327ff55f07906d372b767cffd3bd53422ef952c
kogehipo <ma@ee3.jp>
2024-06-26 17:49:31 +0900

    added member's URL

--- RaspiPico-Thonny/main.py
+++ RaspiPico-Thonny/main.py
@@ -1,9 +1,23 @@
-from machina import Pin
+from machine import Pin, PWM
 import utime
-# PicoではGPIO25にLEDが接続されている
-led = Pin(100, Pin.OUT)
+
+# LEDの接続情報
+led_pin = [2,3,5,6,7,8,9,10]
+led = []
+for i in range(8):
+    led.append(Pin(led_pin[i], Pin.OUT))
+
+# ブザーの接続と音程
+buzzer = PWM(Pin(4))
+freqs = [262,293,329,349,392,440,493,523]
+
+
+
 while True:
-    # LEDの状態を反転(点滅)
-    led.toggle()
-    # 200ms waitする
-    utime.sleep_ms(200)
+    for tone in range(8):
+        led[tone].value(1)			# LEDオン
+        buzzer.freq(freqs[tone])	# ブザーの音程
+        buzzer.duty_u16(12000)		# ブザーを有効化
+        utime.sleep(0.5)
+        buzzer.duty_u16(0)			# ブザーを無効化
+        led[tone].value(0)			# LED オフ
